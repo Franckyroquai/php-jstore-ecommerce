c41e0fa238b812de89f1f0f5a0806c580ec383d2
Franckyroquai <franckyroquai@gmail.com>
2024-04-03 18:35:11 +0200

    mise en place de la vue

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function displayAccueil(){
+    return '<h1>Bienvenu sur la page d\'Accueil</h1>';
+}
+function displayContact(){
+    return '<h1>Bienvenu sur la page de Contact</h1>';
+}
 
 
 
