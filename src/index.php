4fcbac47561df68222a26dac3a54e37343a0ba2f
Franckyroquai <franckyroquai@gmail.com>
2024-04-03 18:10:23 +0200

    mise en place du controller

--- src/index.php
+++ src/index.php
@@ -1,6 +1,21 @@
 <?php
+    require "include.php";
 
-    echo "Fichier Index";
+    $url = trim($_SERVER['PATH_INFO'],'/');
+    $url = explode('/',$url);
+    $route = array("accueil", "contact");
+    //print_r($url);
+
+    $action = $url[0];
+
+    // controller
+    if(!in_array($action,$route)){
+        echo 'error';
+    }else{
+        //echo 'Bienvenue sur la page '.$action;
+        $function = "display".ucwords($action)."()";
+        echo $function;
+    }
 
 
 
