4b8fdc5313ce5894c7fd9e89410ba0bd89bef37a
Franckyroquai <franckyroquai@gmail.com>
2024-04-04 14:35:12 +0200

    import du model

--- src/include.php
+++ src/include.php
@@ -2,8 +2,16 @@
 define("SRC", dirname(__FILE__));
 define("ROOT", dirname(SRC));
 define("SP", DIRECTORY_SEPARATOR);
+define("CONFIG",  ROOT.SP."config");
 define("VIEWS",  ROOT.SP."views");
 define("MODEL",  ROOT.SP."model");
+
+//import du model
+require CONFIG.SP."config.php";
+require MODEL.SP."DataLayer.class.php";
+
+$data = new DataLayer();
+
 
 // les fonctions appelées par le controller
 require "functions.php";
